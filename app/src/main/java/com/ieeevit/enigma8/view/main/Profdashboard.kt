663eaf85b365a897de1f214ee800bf4c6df40b23
jazxii <jassimshamim@gmail.com>
2021-11-07 20:47:07 +0530

    dashboard Mid-Fi UI done

---
+++ app/src/main/java/com/ieeevit/enigma8/view/main/Profdashboard.kt
@@ -0,0 +1,16 @@
+package com.ieeevit.enigma8.view.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ieeevit.enigma8.R
+
+
+
+
+class Profdashboard : Fragment(R.layout.fragment_profile_dashboard) {
+
+    }
+
